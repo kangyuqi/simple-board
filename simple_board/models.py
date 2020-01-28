edbe78575566aa0ea4e2bec976d6bcb1a0caf5ef
Aaron Kang <kangyuqi>
2020-01-28 19:32:02 +0800

    模型；表单；视图；部分注释

--- simple_board/models.py
+++ simple_board/models.py
@@ -1,1 +1,12 @@
+"""
+数据库模型
+"""
 from datetime import datetime
+from simple_board import db
+
+
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(200))
+    name = db.Column(db.String(20))
+    timestamp = db.Column(db.Datetime, default=datetime.now, index=True)
