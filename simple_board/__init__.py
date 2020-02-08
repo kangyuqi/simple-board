b6816eda0c79572ed80badb4d526680627c2c80e
Aaron Kang <kangyuqi>
2020-02-09 00:45:45 +0800

    添加类型注解，开启flask相关自动补全

--- simple_board/__init__.py
+++ simple_board/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 
-app = Flask('simple_board')
+app = Flask('simple_board')  # type:Flask  # 添加类型注解，开启flask相关自动补全
 app.config.from_pyfile('settings.py')
 app.jinja_env.trim_blocks = True
 app.jinja_env.lstrip_blocks = True
